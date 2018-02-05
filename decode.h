8676cd49464eee5438569f763d3cd74bf8f13ea4
Elijah Harmon <elijahharmon@gmail.com>
2018-02-05 10:32:04 -0500

    Decode imported correctly and tested

---
+++ decode.h
@@ -0,0 +1,8 @@
+/*  decode.h  */
+
+#ifndef DECODE_H
+#define DECODE_H
+
+int decode(int argc, char *argv[]);
+
+#endif
--- zergmap.c
+++ zergmap.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
+#include "decode.h"
+
 // This is my main function
-int main(void)
+int main(int argc, char *argv[])
 {
+
+    if (argc < 2)
+    {
+        fprintf(stderr, "Invalid amount of args\n");
+        return 1;
+    }
+
+    decode(argc, argv);
 
     return 0;
 }
